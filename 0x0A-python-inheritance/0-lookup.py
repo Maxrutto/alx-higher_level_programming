e3deafec1d3c8a56370a079b2634206cb8b34255
Maxrutto <ruttomaxmillan@gmail.com>
2023-09-11 09:33:47 -0700

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """ This function defines a list of attributes and methods
+    of an object
+
+    Args:
+        obj: The current object being listed from
+
+
+    Returns:
+        The list of attributes and methods
+
+    """
+    return dir(obj)
