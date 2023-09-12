b1c85b21b3df8ad84e256830f53ed4d7d0a8cd3c
Maxrutto <ruttomaxmillan@gmail.com>
2023-09-12 14:29:32 -0700

    Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module defines a function that returns true if
+an object is an instance of an inherited class
+
+"""
+
+def inherits_from(obj, a_class):
+    """ Definition of the function
+
+    Args:
+        obj: The object being checked
+        a_class: The base class being inherited
+
+    Returns:
+        True: if the object is an instance of the specified
+        False: Otherwise
+
+
+    """
+
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    else:
+        return False
