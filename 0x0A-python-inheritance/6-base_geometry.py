648cdd05486ded3ba7522ac829ac0dd89ea1a133
Maxrutto <ruttomaxmillan@gmail.com>
2023-09-12 14:45:19 -0700

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This is an empty class
+
+"""
+
+
+class BaseGeometry:
+    """Defines a method for area"""
+
+    def area(self):
+        """ Method that returns the area
+
+        Raises:
+            Exception: Area is not implemented
+
+        """
+
+        raise Exception("area() is not implemented")
