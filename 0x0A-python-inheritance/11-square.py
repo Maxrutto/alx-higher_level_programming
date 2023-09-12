4f26da169c78aab427f9678c7b2daa1f90c44c9b
Maxrutto <ruttomaxmillan@gmail.com>
2023-09-12 16:03:07 -0700

    Full square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+This module defines a square with it's attributes
+
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Defines a new child class with Rectangle's attributes
+        but a few changes """
+
+    def __init__(self, size):
+        """ Initializing the attributes
+
+        Args:
+            size: Size of one side
+
+        """
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """ Defines the area of a square
+
+        Returns:
+                The area
+
+        """
+
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        """ This magic method prints the class
+
+        Returns:
+            A string representing the square
+
+        """
+
+        return "[Square] {}/{}".format(self.__size, self.__size)
