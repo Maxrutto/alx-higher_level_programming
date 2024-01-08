f08aeeca28c54287ae67a1cfff06923ccd1168b0
Maxrutto <ruttomaxmillan@gmail.com>
2024-01-08 07:54:15 -0800

    Fixing bugs

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let i;
-stringArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let stringArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (i = 0; i < 3; i++) {
   console.log(stringArray[i]);
