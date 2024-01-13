605b6ad16f92baba1baf4449175ee80d3a1d8b2e
Maxrutto <ruttomaxmillan@gmail.com>
2024-01-13 07:12:30 -0800

    Fixing pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
     """Connection to the database and retrieving the
     states
     """
-    db_connect = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=                                 argv[2], db=argv[3])
+    db_connect = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                                 passwd=argv[2], db=argv[3])
     cur = db_connect.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
@@ -20,4 +21,3 @@ if __name__ == '__main__':
         print(row)
     cur.close()
     db_connect.close()
-
